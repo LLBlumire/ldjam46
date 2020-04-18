2bdaf8d2d0c3367470bb84cb3643c6009116bbc1
Valentin Jenny <jennyv@student.ethz.ch>
2020-04-18 15:33:22 +0200

    Auto stash before rebase of "origin/master"

--- src/World/World.gd
+++ src/World/World.gd
@@ -8,6 +8,5 @@ func _ready():
 	world_tile_map = get_tree().get_root().get_node("World/WorldMap/WorldTileMap")
 	print("HEllo, World")
 	print(world_tile_map.map_to_world(Vector2(3, -1)))
-
 func _process(delta):
 	pass

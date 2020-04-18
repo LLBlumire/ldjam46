6947c5f63a532da79f098e0ab332b16927ad26bb
Lucille Blumire <llblumire@gmail.com>
2020-04-18 13:53:32 +0100

    Expose WorldMap in World

--- src/World/World.gd
+++ src/World/World.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+var world_cursor: WorldEdit
+var world_tile_map: TileMap
+
 func _ready():
-	pass
+	world_cursor = get_tree().get_root().get_node("World/WorldMap/WorldSelect")
+	world_tile_map = get_tree().get_root().get_node("World/WorldMap/WorldTileMap")
 
 func _process(delta):
 	pass

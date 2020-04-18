8921206b0147450004668bf68cf641a40d10e65d
Lucille Blumire <llblumire@gmail.com>
2020-04-18 14:29:52 +0100

    Add World Tile Map Coordinate Test

--- src/World/World.gd
+++ src/World/World.gd
@@ -6,6 +6,8 @@ var world_tile_map: TileMap
 func _ready():
 	world_cursor = get_tree().get_root().get_node("World/WorldMap/WorldSelect")
 	world_tile_map = get_tree().get_root().get_node("World/WorldMap/WorldTileMap")
+	print("HEllo, World")
+	print(world_tile_map.map_to_world(Vector2(3, -1)))
 
 func _process(delta):
 	pass
